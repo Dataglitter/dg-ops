594d861c09b3f10871644163e5b8bc18ec933349
Sangram Reddy <reddy.horcrux@gmail.com>
2018-12-08 00:41:20 +0000

    refactored shell scripts

--- assets-bucket.sh
+++
@@ -1,7 +0,0 @@
-DATAGLITTER_ASSETS_S3_BUCKET={DATAGLITTER_ASSETS_S3_BUCKET:-assets}
-
-aws cloudformation $1-stack \
-    --stack-name dataglitter-assets \
-    --template-body file://${PWD}/cf/assets-bucket-setup.yaml \
-    --parameters ParameterKey=AssetsBucketName,ParameterValue=$DATAGLITTER_ASSETS_S3_BUCKET \
-    --profile prod
